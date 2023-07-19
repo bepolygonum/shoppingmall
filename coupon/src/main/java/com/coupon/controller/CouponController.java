72e6bbc43c49e75c0849ad8c4bd773822b14451c
kxy <961646438@qq.com>
2023-07-19 14:32:28 +0800

    nacos config

--- coupon/src/main/java/com/coupon/controller/CouponController.java
+++ coupon/src/main/java/com/coupon/controller/CouponController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,11 +27,22 @@ import com.common.utils.R;
  * @email polygonumkxy@163.com
  * @date 2023-06-14 14:06:31
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("/testconfig")
+    public R testconfig(){
+
+        return R.ok().put("name",name).put("age",age);
+    }
 
     @RequestMapping("/member/list")
     public R getMemberCoupons(){
